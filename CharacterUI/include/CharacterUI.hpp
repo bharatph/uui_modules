de6040194585f1a8e92a585a14804db5e73fa3c5
Bharatvaj <bharatvajph@gmail.com>
2018-03-21 20:00:02 +0530

    Vision level #6

--- CharacterUI/include/CharacterUI.hpp
+++ CharacterUI/include/CharacterUI.hpp
@@ -1,15 +1,24 @@
-#include <UniversalUI.hpp>
 #include <iostream>
+#include <UniversalUI.hpp>
 
 #include "config.h"
 
 using namespace std;
-using namespace uui;
 
-class CharacterUI : public UI {
-public:
-  CharacterUI();
-  virtual void error(std::string msg, std::string ok_msg = "OK") /*override*/;
-  virtual int alert(std::string, uui::action = PROMPT, std::string = "OK",
-                    std::string = "Cancel") /*override*/;
+namespace uui
+{
+class CharacterUI : public UI
+{
+  private:
+	static CharacterUI *instance;
+
+  public:
+	static CharacterUI *getInstance();
+	virtual void error(std::string msg, std::string ok_msg = "OK") /*override*/;
+	virtual int alert(std::string, uui::action = PROMPT, std::string = "OK",
+					  std::string = "Cancel") /*override*/;
+
+  protected:
+	CharacterUI();
 };
+}
--- CharacterUI/src/CharacterUI.cpp
+++ CharacterUI/src/CharacterUI.cpp
@@ -1,10 +1,26 @@
 #include <CharacterUI.hpp>
 
-CharacterUI::CharacterUI() {}
-void CharacterUI::error(std::string msg, std::string ok_msg) {
-  std::cout << msg << std::endl;
+uui::CharacterUI *uui::CharacterUI::instance = nullptr;
+
+uui::CharacterUI::CharacterUI()
+{
 }
-int CharacterUI::alert(std::string msg, uui::action ac, std::string ok_msg,
-                       std::string cancel_msg) {
-  std::cout << msg << std::endl;
+uui::CharacterUI *uui::CharacterUI::getInstance()
+{
+	if (instance == nullptr)
+	{
+		instance = new uui::CharacterUI();
+		map<std::string, std::string> commands;
+	}
+	return instance;
+}
+
+void uui::CharacterUI::error(std::string msg, std::string ok_msg)
+{
+	std::cout << msg << std::endl;
+}
+int uui::CharacterUI::alert(std::string msg, uui::action ac, std::string ok_msg,
+							std::string cancel_msg)
+{
+	std::cout << msg << std::endl;
 }
